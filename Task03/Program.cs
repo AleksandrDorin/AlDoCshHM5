dfa6f955be4f5c2d2485e445de2b2716263091ee
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 19:25:32 +0300

    Task03 A beautiful year done

---
+++ Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Красивый год
+int year = int.Parse(Console.ReadLine());
+
+void NextDiffYear(int year)
+{
+    for (int y = year + 1; ; y++)
+    {
+        int a = y / 1000;
+        int b = y / 100 % 10;
+        int c = y / 10 % 10;
+        int d = y % 10;
+        if (a != b && b != c && c != d && a != c && a != d && b != d)
+        {
+          Console.WriteLine(y); 
+            return;
+        }
+    }
+    
+}
+NextDiffYear(year);
