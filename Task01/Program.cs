6c9782a4a1ab3751c27bf0556af2d824c75017b5
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 16:43:32 +0300

    Task Theater Square done

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Театральная площадь
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
+
+int x, y;
+x = n / a;
+y = m / a;
+if (n % a != 0)
+    x++;
+if (m % a != 0)
+    y++;
+Console.WriteLine(x * y);
