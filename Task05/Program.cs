e135d2d8ccd82f8a59cbe5df06b35923852d2f9b
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 20:57:49 +0300

    Task03 A beautiful year done

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿// Солдат и бананы
+
+
+Console.Clear();
+Console.Write("Цена за первый банан: ");
+int k = int.Parse(Console.ReadLine());
+Console.Write("Нужно купить бананов: ");
+int w = int.Parse(Console.ReadLine());
+Console.Write("У солдата $ : ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+int res = 0;
+while (i <= w)
+{
+    res += k * i;
+    i++;
+}
+Console.Write($"У солдата {n}$, нужная сумма {res}$, он должен одолжить у однополчанина {res - n}$ ");
