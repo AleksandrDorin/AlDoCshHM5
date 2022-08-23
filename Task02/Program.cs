e48248794c76f969509fbec22b1d39073d313a3d
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 18:54:22 +0300

    Task Elephant done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,34 +1,10 @@
 ﻿// Слоник
 
 int x = int.Parse(Console.ReadLine());
-int i = 0;
-while (i <= x)
+int k = 0;
+for (int i = 5; i > 0; i--)
 {
-    if (x >= 5)
-    {
-        x -= 5;
-        i++;
-    }
-    else if (x >= 4)
-    {
-        x -= 4;
-        i++;
-    }
-    else if (x >= 3)
-    {
-        x -= 3;
-        i++;
-    }
-    else if (x >= 2)
-    {
-        x -= 2;
-        i++;
-    }
-    else if (x >= 1)
-    {
-        x -= 1;
-        i++;
-    }
+    k += x / i;
+    x = x % i;
 }
-
-Console.WriteLine(i);
+Console.WriteLine(k);
