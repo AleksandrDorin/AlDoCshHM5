334eeac91804c869e0398ed71f5ce166477adcd1
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-23 17:12:59 +0300

    Task Elephant done

---
+++ Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Слоник
+
+int x = int.Parse(Console.ReadLine());
+int i = 0;
+while (i <= x)
+{
+    if (x >= 5)
+    {
+        x -= 5;
+        i++;
+    }
+    else if (x >= 4)
+    {
+        x -= 4;
+        i++;
+    }
+    else if (x >= 3)
+    {
+        x -= 3;
+        i++;
+    }
+    else if (x >= 2)
+    {
+        x -= 2;
+        i++;
+    }
+    else if (x >= 1)
+    {
+        x -= 1;
+        i++;
+    }
+}
+
+Console.WriteLine(i);
